863d0bf7bb7263a1ee0caa164fc7d02d50d2d1b3
leodini <leodini@hotmail.com>
2020-07-05 08:36:34 -0300

    masterhead of user page with random image display

---
+++ client/src/components/User/StyledUser.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const MasterHead = styled.div`
+  margin: 40px 0;
+  height: 500px;
+
+  .mt-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .mt-container .user-mt {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .user-mt h1 {
+    color: #0d0c22;
+    margin-bottom: 12px;
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 38px;
+  }
+
+  .user-mt h2 {
+    color: #0d0c22;
+
+    font-size: 48px;
+    font-weight: 700;
+    line-height: 56px;
+  }
+
+  .mt-container .img-container img {
+    height: 400px;
+  }
+
+  .img-container .masterhead-block-container {
+    width: 30%;
+    height: 45%;
+    position: absolute;
+    right: 120px;
+    top: -40px;
+    z-index: -1;
+    background: #ea4c89;
+  }
+
+  .img-container .masterhead-block-container .block {
+  }
+`;
--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PostContainer from "../Posts/PostContainer";
 import Header from "../Header/Header";
+import { MasterHead } from "./StyledUser";
+import Avatar from "../Shared/Avatar";
 import api from "../../api";
 
 const User = () => {
@@ -17,14 +19,36 @@ const User = () => {
     setUserData(data);
   };
 
+  const randomImage = (ceil) => {
+    if (typeof ceil === "number") {
+      return Math.floor(Math.random() * ceil + 1);
+    }
+    throw new Error("must provide a number");
+  };
+
   if (!userData) return null;
   const { username, posts, bio, liked, followers, following } = userData;
+  const randomNumber = randomImage(posts.length);
+  const masterHeadImage = posts[randomNumber].image_url;
   return (
     <>
       <Header />
       <div className="container">
-        <h1>{username}</h1>
-        <h2>{bio}</h2>
+        <MasterHead>
+          <div className="mt-container">
+            <div className="user-mt">
+              {/* <Avatar /> */}
+              <h1>{username}</h1>
+              <h2>{bio}</h2>
+            </div>
+            <div className="img-container">
+              <div className="masterhead-block-container">
+                <div className="block"></div>
+              </div>
+              <img src={masterHeadImage} alt={username} />
+            </div>
+          </div>
+        </MasterHead>
         <PostContainer data={posts} />
       </div>
     </>
